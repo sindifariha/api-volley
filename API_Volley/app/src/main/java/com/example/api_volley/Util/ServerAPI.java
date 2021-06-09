a493ff09a3f5cd490bd3af17587f59f74021a51d
unknown <80382156+sindifariha@users.noreply.github.com>
2021-06-09 21:58:36 +0700

    api_volley

---
+++ API_Volley/app/src/main/java/com/example/api_volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.api_volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.100.19/crud/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.100.19/crud/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.100.19/crud/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.100.19/crud/Mahasiswa/ApiUpdate";
+}
